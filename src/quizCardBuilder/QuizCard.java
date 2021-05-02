71d1e7e70fe1ed4cd0a5b85bab17df09125fefc6
Pavel Liakhau <liakhauPavel1991@yandex.by>
2021-05-02 17:29:23 +0300

    Creating cards

---
+++ src/quizCardBuilder/QuizCard.java
@@ -0,0 +1,29 @@
+package quizCardBuilder;
+
+public class QuizCard {
+
+    String question;
+    String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
